559f847e89739624f3972500239be413ba91080d
je-swy <kateryna.shemonaieva@gmail.com>
2025-09-30 12:57:27 +0300

    modal block final fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 // src/js/main.js
-// For all non-home pages: just load header/footer.
 
 import { includeComponents } from './components-loader.js';
 import { initHomepage } from './homepage.js';
@@ -8,10 +7,10 @@ import { initProductDetails } from './product-details.js';
 
 // Єдина точка входу в додаток
 document.addEventListener('DOMContentLoaded', async () => {
-  // 1. Завжди завантажуємо хедер і футер
+  // 1. Завжди завантажуємо хедер і футер ПЕРШИМ
   await includeComponents();
 
-  // 2. Визначаємо, на якій ми сторінці, і запускаємо її унікальний скрипт
+  // 2. Визначаємо, на якій ми сторінці, і запускаємо ТІЛЬКИ її унікальний скрипт
   if (document.querySelector('.homepage_promo')) {
     initHomepage();
   } else if (document.querySelector('.catalog')) {
